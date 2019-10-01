278564854590d3eca1201e7361bbe5654795eddd
bdbai <bdbaiapp@163.com>
2019-10-02 02:16:11 +0800

    Fix mac addr lookup

--- src/mac.rs
+++ src/mac.rs
@@ -1,72 +1,45 @@
 use {
-    crate::LIB,
     pnet::datalink::MacAddr,
-    rawsock::traits::Library,
-    std::{
-        fs::File,
-        io::{BufRead, BufReader},
-        net::{IpAddr, Ipv4Addr, Ipv6Addr},
-        str::FromStr,
-    },
+    std::{process::Command, str::FromStr},
 };
 
-#[derive(Debug, Clone)]
-struct ArpEntry {
-    ip_addr: IpAddr,
-    // hw_type: u32,
-    // flags: u32,
-    hw_addr: MacAddr,
-    // mask: String,
-    // device: String
+fn get_default_dest(netif_name: &str) -> Option<String> {
+    let output = Command::new("ip")
+        .arg("route")
+        .arg("show")
+        .arg("dev")
+        .arg(netif_name)
+        .output()
+        .expect("Cannot execute ip command");
+    let output = String::from_utf8(output.stdout).unwrap();
+    output.lines().find_map(|line| {
+        if line.starts_with("default") {
+            let fields: Vec<&str> = line.split_whitespace().collect();
+            Some(fields[2].to_owned())
+        } else {
+            None
+        }
+    })
 }
 
-fn get_arp_entries(netif_name: &str) -> Vec<ArpEntry> {
-    let file = File::open("/proc/net/arp").expect("Cannot open arp file");
-    let mut reader = BufReader::new(file);
-    let mut buf = String::with_capacity(100);
-    reader.read_line(&mut buf).unwrap();
-    buf.clear();
-    let mut ret = vec![];
-    while let Ok(size) = reader.read_line(&mut buf) {
-        if size == 0 {
-            break;
-        }
-        let fields: Vec<&str> = buf.split_whitespace().collect();
-        assert_eq!(
-            fields.len(),
-            6,
-            "An ARP entry should have at least 5 fields"
-        );
-        if (fields[2] == "0x2" || fields[2] == "0x6") && fields[5] == netif_name {
-            ret.push(ArpEntry {
-                ip_addr: Ipv4Addr::from_str(fields[0])
-                    .map(|ipv4| IpAddr::V4(ipv4))
-                    .unwrap_or_else(|_| IpAddr::V6(Ipv6Addr::from_str(fields[0]).unwrap())),
-                hw_addr: MacAddr::from_str(fields[3]).unwrap(),
-            });
-        }
-        buf.clear();
-    }
-    ret
+fn get_arp_entry(ip: &str) -> Option<MacAddr> {
+    let output = Command::new("ip")
+        .arg("neigh")
+        .arg("show")
+        .arg("to")
+        .arg(ip)
+        .output()
+        .expect("Cannot execute ip command");
+    let output = String::from_utf8(output.stdout).unwrap();
+    output.lines().find_map(|line| {
+        let fields: Vec<&str> = line.split_whitespace().collect();
+        fields
+            .get(4)
+            .map(|s| MacAddr::from_str(s).expect("Invalid MAC address"))
+    })
 }
 
 pub fn get_dst_address_by_netif_name(netif_name: &str) -> Option<MacAddr> {
-    let netif = LIB
-        .get()
-        .unwrap()
-        .all_interfaces()
-        .unwrap()
-        .into_iter()
-        .find(|netif| netif.name == netif_name)
-        .unwrap();
-    let addresses = netif.addresses.unwrap();
-    let arp_entries = get_arp_entries(netif_name);
-    arp_entries
-        .into_iter()
-        .find(|arp| {
-            addresses
-                .iter()
-                .any(|addr| addr.address.map_or(false, |addr| addr.ip() != arp.ip_addr))
-        })
-        .map(|addr| addr.hw_addr)
+    let dest = get_default_dest(netif_name)?;
+    get_arp_entry(dest.as_str())
 }
